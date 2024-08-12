e382ddb223732ede3013d025abc5796fb47f56e4
imohamedmahgoub <imohamedmahgoub@gmail.com>
2024-08-12 22:41:14 +0300

    initial commit

---
+++ Sports/Modules/Leagues/view/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  Sports
+//
+//  Created by Mohamed Mahgoub on 12/08/2024.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
